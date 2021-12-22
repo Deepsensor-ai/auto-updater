898cdcd9906030b645e3e74d58b470d0b6b0019e
Munish <info@deepsensor.ai>
2021-12-21 23:42:32 -0500

    new

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
+
+
 let mainWindow;
+process.env.NODE_ENV = 'production' 
+require('update-electron-app')()
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -11,43 +15,45 @@ function createWindow () {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile('test.html');
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
 }
 
-mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-});
+// mainWindow.once('ready-to-show', () => {
+//     console.log("Show..")
+//     mainWindow.show = true
+//    // autoUpdater.checkForUpdatesAndNotify();
+// });
 
 
 app.on('ready', () => {
   createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+// app.on('activate', function () {
+//   if (mainWindow === null) {
+//     createWindow();
+//   }
+// });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
+// ipcMain.on('app_version', (event) => {
+//   event.sender.send('app_version', { version: app.getVersion() });
+// });
 
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-  });
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-  autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
+// ipcMain.on('restart_app', () => {
+//     autoUpdater.quitAndInstall();
+//   });
+// autoUpdater.on('update-available', () => {
+//     mainWindow.webContents.send('update_available');
+//   });
+//   autoUpdater.on('update-downloaded', () => {
+//     mainWindow.webContents.send('update_downloaded');
+//   });
